44b7c27017c2886dcb567a210e4ba1582e5206db
Diego Alvarado <diego.alvarado.m@usach.cl>
2023-12-18 02:50:20 -0300

    Actualización del repositorio V2
    Falta conexión con back puesto a que ms tasks no esta funcionado. Se dejo la estructura lista para que cuando este en marcha se realice la conexión

---
+++ src/datosTabla/datosBack.tsx
@@ -0,0 +1,66 @@
+export const tareasUsuario = [
+    {
+        "description": "Tarea pro 2 reu 1",
+        "type": "compromiso",
+        "participants": ["diego.alvarado.m@usach.cl"],
+        "topic": 0,
+        "meeting": [
+            "657f4b2287f194cf72f4855b"
+        ],
+        "project": [
+            "657f48ef098b0f67b96e4c21"
+        ],
+        "state": "Nueva",
+        "number": 0,
+        "dateLimit": "31-12-2023",
+        "postition": "17-12-2023"
+    },
+    {
+        "description": "Tarea pro 2 reu 2",
+        "type": "compromiso",
+        "participants": ["diego.alvarado.m@usach.cl"],
+        "topic": 0,
+        "meeting": [
+            "657f4b2887f194cf72f4855d"
+        ],
+        "project": [
+            "657f48ef098b0f67b96e4c21"
+        ],
+        "state": "Nueva",
+        "number": 0,
+        "dateLimit": "31-12-2023",
+        "postition": "17-12-2023"
+    },
+    {
+        "description": "Tarea pro 3 reu 1",
+        "type": "compromiso",
+        "participants": ["diego.alvarado.m@usach.cl"],
+        "topic": 0,
+        "meeting": [
+            "657f4b4487f194cf72f48561"
+        ],
+        "project": [
+            "657f48fc098b0f67b96e4c23"
+        ],
+        "state": "Nueva",
+        "number": 0,
+        "dateLimit": "31-12-2023",
+        "postition": "17-12-2023"
+    },
+    {
+        "description": "Tarea pro 3 reu 2",
+        "type": "compromiso",
+        "participants": ["diego.alvarado.m@usach.cl"],
+        "topic": 0,
+        "meeting": [
+            "657f4b5087f194cf72f48563"
+        ],
+        "project": [
+            "657f48fc098b0f67b96e4c23"
+        ],
+        "state": "Nueva",
+        "number": 0,
+        "dateLimit": "31-12-2023",
+        "postition": "17-12-2023"
+    },    
+    ];
--- src/datosTabla/sample-data.tsx
+++ src/datosTabla/sample-data.tsx
@@ -5,16 +5,26 @@ import Avatar from '@atlaskit/avatar';
 import DropdownMenu, {
   DropdownItem,
   DropdownItemGroup,
+  DropdownItemRadio,
+  DropdownItemRadioGroup,
 } from '@atlaskit/dropdown-menu';
 import { token } from '@atlaskit/tokens';
-
+import axios from 'axios';
 import { lorem } from './lorem';
-import { presidents } from './presidents';
+import { tareasUsuario } from './datosBack';
 
-interface President {
-  id: number;
-  name: string;
-  party: string;
+interface TareasUsuario {
+  description: string;
+  type: string;
+  participants: Array<string>;
+  topic: number;
+  meeting: Array<string>;
+  project: Array<string>;
+  state: string;
+  number: number;
+  dateLimit: string;
+  postition: string;
+  nombre?: string;
 }
 
 function createKey(input: string) {
@@ -25,43 +35,50 @@ function iterateThroughLorem(index: number) {
   return index > lorem.length ? index - lorem.length : index;
 }
 
-const nameWrapperStyles = css({
-  display: 'flex',
-  alignItems: 'center',
-});
+// const nameWrapperStyles = css({
+//   display: 'flex',
+//   alignItems: 'center',
+// });
 
-const NameWrapper: FC<{ children: ReactNode }> = ({ children }) => (
-  <span css={nameWrapperStyles}>{children}</span>
-);
+// const NameWrapper: FC<{ children: ReactNode }> = ({ children }) => (
+//   <span css={nameWrapperStyles}>{children}</span>
+// );
 
-const avatarWrapperStyles = css({
-  marginRight: token('space.100', '8px'),
-});
+// const avatarWrapperStyles = css({
+//   marginRight: token('space.100', '8px'),
+// });
 
-const AvatarWrapper: FC<{ children: ReactNode }> = ({ children }) => (
-  <div css={avatarWrapperStyles}>{children}</div>
-);
+// const AvatarWrapper: FC<{ children: ReactNode }> = ({ children }) => (
+//   <div css={avatarWrapperStyles}>{children}</div>
+// );
 
-export const caption = 'List of US Presidents';
+export const caption = 'List of Tareas de Usuario';
 
 export const createHead = (withWidth: boolean) => {
   return {
     cells: [
       {
-        key: 'name',
+        key: 'participants',
         content: 'Encargado',
         isSortable: true,
-        width: withWidth ? 25 : undefined,
+        width: withWidth ? 15 : undefined,
       },
       {
-        key: 'party',
+        key: 'project',
         content: 'Proyecto',
         shouldTruncate: true,
         isSortable: true,
         width: withWidth ? 15 : undefined,
       },
       {
-        key: 'more',
+        key: 'meeting',
+        content: 'Reunión',
+        shouldTruncate: true,
+        isSortable: true,
+        width: withWidth ? 15 : undefined,
+      },
+      {
+        key: 'state',
         content: 'Estado',
         shouldTruncate: true,
         width: withWidth ? 15 : undefined,
@@ -71,40 +88,118 @@ export const createHead = (withWidth: boolean) => {
 };
 
 export const head = createHead(true);
+// export const rows = tareasUsuario.map((tarea: TareasUsuario, index: number) => ({
+//   key: `row-${index}-${tarea.name}`,
+//   isHighlighted: false,
+//   cells: [
+//     {
+//       key: 'name',
+//       content: (
+//         <NameWrapper>
+//           <AvatarWrapper>
+//             <Avatar name={tarea.name} size="medium" />
+//           </AvatarWrapper>
+//           <a href="">{tarea.name}</a>
+//         </NameWrapper>
+//       ),
+//     },
+//     {
+//       key: 'project',
+//       content: tarea.project.join(', '),
+//     },
+//     {
+//       key: 'state',
+//       content: tarea.state,
+//     },
+//   ],
+// }));
+const tokenAcceso = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRpZWdvLmFsdmFyYWRvLm1AdXNhY2guY2wiLCJpYXQiOjE3MDI4NjU3MTMsImV4cCI6MTcxMzY2NTcxMywiYXVkIjoiaHR0cHM6Ly9tZWV0Zmxvdy5jb20ifQ.H28xmBlggUO4MPneLuNTyfTVc6cCRtqrNgOvDiwUDdQ";
+let responseProyectos: any[] = []; // Variable global para almacenar responseProyectos
+let responseReuniones: any[] = []; // Variable global para almacenar responseReuniones
+let nombreProyecto = [];
 
-export const rows = presidents.map((president: President, index: number) => ({
-  key: `row-${index}-${president.name}`,
+
+async function getProjectById(idProyecto: string) {
+  try {
+    const response = await axios.get(`http://172.111.10.181/api/project/getProjectbyID/${idProyecto}` , {
+      headers: {
+        Authorization: `Bearer ${tokenAcceso}`
+        }
+      });
+
+      nombreProyecto = response.data.name;
+    console.log('nombre del proyecto:', nombreProyecto, typeof nombreProyecto);
+    return nombreProyecto;
+  } catch (error) {
+    console.error('Error fetching project:', error);
+    throw error;
+  }
+}
+
+
+
+async function getProjectByIdVER2(tareaU: TareasUsuario) {
+  try {
+    const response = await axios.get(`http://172.111.10.181/api/project/getProjectbyID/${tareaU.project[0]}` , {
+      headers: {
+        Authorization: `Bearer ${tokenAcceso}`
+        }
+      });
+
+      tareaU.project = response.data.name;
+      tareaU.nombre = tareaU.project.toString();
+      console.log('nombre del proyecto:', tareaU.project, typeof tareaU.project);
+      return tareaU;
+  
+  } catch (error) {
+    console.error('Error fetching project:', error);
+    throw error;
+  }
+}
+
+
+
+
+export const rows = tareasUsuario.map((tarea: TareasUsuario, index: number) => ({
+  //recuperar el nombre del proyecto
+  
+  tarea: getProjectByIdVER2(tarea),
+  
+
+  //recuperar el nombre de la reunion
+
+
+  key: `row-${index}-${tarea.participants}`,
   isHighlighted: false,
   cells: [
     {
-      key: createKey(president.name),
-      content: (
-        <NameWrapper>
-          <AvatarWrapper>
-            <Avatar name={president.name} size="medium" />
-          </AvatarWrapper>
-          <a href="">{president.name}</a>
-        </NameWrapper>
-      ),
+      key: 'participants',
+      content: tarea.participants.join(', '),
+      // content: (
+      // content: (
+      //   <NameWrapper>
+      //     <AvatarWrapper>
+      //       <Avatar name={tarea.participants} size="medium" />
+      //     </AvatarWrapper>
+      //     <a href="">{tarea.participants}</a>
+      //   </NameWrapper>
+      // ),
     },
     {
-      key: createKey(president.party),
-      content: president.party,
+      key: 'project',
+      // content: nombreProyecto,
+      content: tarea.nombre!,
     },
-    /*{
-      key: president.id,
-    },*/
-    /*{
-      key: 'Lorem',
-      content: iterateThroughLorem(index),
-    },*/
-
     {
-      key: 'MoreDropdown',
+      key: 'meeting',
+      content: tarea.meeting.join(', '),
+    },
+    {
+      key: 'state',
       content: (
-        <DropdownMenu trigger="No atendida">
+        <DropdownMenu trigger={tarea.state}>
           <DropdownItemGroup>
-            <DropdownItem>Atendida</DropdownItem>
+            <DropdownItem >Atendida</DropdownItem>
             <DropdownItem>No atendida</DropdownItem>
           </DropdownItemGroup>
         </DropdownMenu>
